b154bdb0040ff726c9c853a8938f9a2eedda31bf
cloudwebrtc <duanweiwei1982@gmail.com>
2022-09-29 12:54:49 +0800

    add simple app without sdk for iOS.

---
+++ SimpleAppiOS/SimpleAppiOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SimpleAppiOS
+//
+//  Created by 段维伟 on 2022/9/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleAppiOS/SimpleAppiOS/SimpleAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppiOSApp.swift
+//  SimpleAppiOS
+//
+//  Created by 段维伟 on 2022/9/29.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
